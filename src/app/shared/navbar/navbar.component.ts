564ddc1fb84742a066b925e377850cc05d853360
Leonard Velásquez <leonard.avp@gmail.com>
2022-11-08 20:21:41 -0400

    Correccion de campos y logout

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs';
+import { LOGOUT_QUERY } from 'src/app/graphql/queries';
 import { ValidRoles } from 'src/app/interfaces/user.interface';
 import { AuthService } from 'src/app/services/auth.service';
+import { GraphqlService } from 'src/app/services/graphql.service';
 import { LoadingService } from 'src/app/services/loading.service';
 import { NotifyService } from 'src/app/services/notify.service';
 import { Options } from '../interfaces/nav-options.interface';
@@ -17,7 +18,7 @@ import { Options } from '../interfaces/nav-options.interface';
 export class NavbarComponent implements OnInit {
 
   constructor(
-    private http: HttpClient,
+    private graphql: GraphqlService,
     private router: Router,
     private notifyService: NotifyService,
     private auth: AuthService,
@@ -27,66 +28,68 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  logout(){
+  logout() {
     this.loading.displayLoading('Cerrando sesión...')
-    this.http.post('$/users/logout',{})
-    .subscribe({
-      next:()=>{
-        this.redirect()
-        this.loading.hideLoading()
-      },
-      error:(error)=>{
-        this.loading.hideLoading()
-        if(error.error.statusCode === 401){
-          return this.redirect()
-        } else{
-          this.redirect()
-          console.log(error)
+    this.graphql.query<{ logout: { success: boolean } }, never>(LOGOUT_QUERY)
+      .subscribe(
+        {
+          next: () => {
+            this.redirect()
+            this.loading.hideLoading()
+          },
+          error: (error) => {
+            this.loading.hideLoading()            
+            if (error.error.statusCode === 401) {
+              return this.redirect()
+            } else {
+              this.redirect()
+              console.log(error)
+            }
+          }
         }
-      }
-    })
+      )
   }
 
-  private redirect(){
+  private redirect() {
     this.notifyService.success('Sesión cerrada con exito!')
-    this.router.navigate(['auth','login'])
+    this.router.navigate(['auth', 'login'])
   }
 
-  public get isAdmin() : boolean {
+  public get isAdmin(): boolean {
     return this.auth.rol === ValidRoles.admin
   }
 
-  public get habitacionesOptions(): Options[]{
-    const {rol} = this.auth
+  public get habitacionesOptions(): Options[] {
+    const { rol } = this.auth
     const coreResult = [
-      {label: 'Reservar habitación', icon:'bed',route:'main/habitaciones/reservar'},
-      {label: 'Estado de alquileres', icon:'bed',route:'main/habitaciones/alquileres'},
+      { label: 'Reservar habitación', icon: 'bed', route: 'main/habitaciones/reservar' },
+      { label: 'Estado de alquileres', icon: 'bed', route: 'main/habitaciones/alquileres' },
     ]
-    if(rol === ValidRoles.admin){
+    if (rol === ValidRoles.admin) {
       coreResult.push(
-        {label: 'Registrar habitaciones', icon:'hotel',route:'main/habitaciones/registrar'},
-        {label: 'Consultar habitaciones', icon:'hotel',route:'main/habitaciones'},
+        { label: 'Registrar habitaciones', icon: 'hotel', route: 'main/habitaciones/registrar' },
+        { label: 'Consultar habitaciones', icon: 'hotel', route: 'main/habitaciones' },
       )
     }
     return coreResult
   }
 
   inventarioOptions: Options[] = [
-    {label: 'Añadir producto', icon:'add',route:'main/inventario/registrar'},
-    {label: 'Consultar productos', icon:'search',route:'main/inventario'},
-    {label: 'Consultar stock de inventario', icon:'inventory_2',route:'main/inventario/stock'},
-    {label: 'Registrar compra', icon:'add_shopping_cart',route:'main/inventario/compras/registrar'},
-    {label: 'Consultar compras', icon:'shopping_cart',route:'main/inventario/compras'},
+    { label: 'Añadir producto', icon: 'add', route: 'main/inventario/registrar' },
+    { label: 'Consultar productos', icon: 'search', route: 'main/inventario' },
+    { label: 'Consultar stock de inventario', icon: 'inventory_2', route: 'main/inventario/stock' },
+    { label: 'Registrar compra', icon: 'add_shopping_cart', route: 'main/inventario/compras/registrar' },
+    { label: 'Consultar compras', icon: 'shopping_cart', route: 'main/inventario/compras' },
   ]
 
   proveedoresOptions: Options[] = [
-    {label: 'Añadir proveedor', icon:'person_add_alt_1',route:'main/proveedores/registrar'},
-    {label: 'Consultar proveedores', icon:'person',route:'main/proveedores'},
+    { label: 'Añadir proveedor', icon: 'person_add_alt_1', route: 'main/proveedores/registrar' },
+    { label: 'Consultar proveedores', icon: 'person', route: 'main/proveedores' },
   ]
 
   adminOptions: Options[] = [
-    {label: 'Usuarios', icon:'person', route:'main/admin/usuarios'},
+    { label: 'Usuarios', icon: 'person', route: 'main/admin/usuarios' },
   ]
 
-  
+
 }
