9a8810825d84b80bde3eddcff50e5bf5573fbf3a
Leonard Velásquez <leonard.avp@gmail.com>
2022-11-15 22:54:37 -0400

    Eliminacion de una habitacion via su pagina

--- src/app/habitaciones/habitacion/habitacion.component.ts
+++ src/app/habitaciones/habitacion/habitacion.component.ts
@@ -3,7 +3,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Alquiler } from 'src/app/interfaces/alquiler.interface';
 import { Habitacion } from 'src/app/interfaces/habitacion.interface';
 import { AuthService } from 'src/app/services/auth.service';
+import { ConfirmService } from 'src/app/services/confirm.service';
+import { NotifyService } from 'src/app/services/notify.service';
 import { AlquilerService } from '../services/alquiler.service';
+import { HabitacionService } from '../services/habitacion.service';
 
 @Component({
   selector: 'hwp-habitacion',
@@ -22,7 +25,10 @@ export class HabitacionComponent implements OnInit {
     private alquilerService: AlquilerService,
     private auth: AuthService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private habitacionService: HabitacionService,
+    private confirm: ConfirmService,
+    private notify: NotifyService,
   ) { }
 
   ngOnInit(): void {
@@ -52,6 +58,23 @@ export class HabitacionComponent implements OnInit {
 
   getDateFromData(dateAsString: string){
     return new Date(+dateAsString)
+  }
+
+  eliminarHabitacion(){
+    this.confirm.danger({
+      title:'Eliminar habitación N° ' + this.habitacion.numero,
+      message:'Esta seguro de querer eliminar esta habitación? Tenga en cuenta que eliminar está habitación tambien eliminará los alquileres y reportes financieros de la misma',
+      okText:'Eliminar',
+      onOk:()=>{
+        this.habitacionService.delete(this.habitacion._id)
+        .subscribe((response)=>{
+          if (response.data?.removeHabitacion._id) {
+            this.notify.success('Habitación eliminada con exito!')
+            this.router.navigate(['/main','habitaciones'])
+          }
+        })
+      }
+    })
   }
 
   get isAdmin():boolean{
