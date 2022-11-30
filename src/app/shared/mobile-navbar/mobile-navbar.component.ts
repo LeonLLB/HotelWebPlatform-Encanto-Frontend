d3cb716dc0c4ac97efb1c195a15dca1c763983e3
Leonard Velásquez <leonard.avp@gmail.com>
2022-11-29 21:11:57 -0400

    Responsive NavBar

--- src/app/shared/mobile-navbar/mobile-navbar.component.ts
+++ src/app/shared/mobile-navbar/mobile-navbar.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { NavbarDataService } from 'src/app/services/navbar-data.service';
+import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
   selector: 'hwp-mobile-navbar',
@@ -7,15 +9,47 @@ import { NavbarDataService } from 'src/app/services/navbar-data.service';
   styles: [
   ]
 })
-export class MobileNavbarComponent{
+export class MobileNavbarComponent implements OnInit, OnDestroy{
 
   isOpen = false
+  isClosing = false
+  hasOpened = false
+
+  navStatusSub!: Subscription
 
   constructor(
-    public navbarData: NavbarDataService
-  ) { }
+    public navbarData: NavbarDataService,
+    private navService: NavigationService
+  ) { }  
 
-  toggleNavbar(){
+  ngOnInit(): void {
+    this.navStatusSub = this.navService.$eventEmitter.subscribe({
+      next:(activeItem)=>{
+        if(activeItem === '' && this.hasOpened){
+          this.isClosing=true
+          setTimeout(()=>{
+            this.isClosing=false    
+            this.isOpen = false
+            setTimeout(()=>{this.hasOpened = false},50)
+          },350)
+        }
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.navStatusSub.unsubscribe()
+  }
+
+  toggleNavbar(){    
+    if(this.isOpen){
+      this.isClosing = true
+      setTimeout(()=>{this.hasOpened = false},50)
+      setTimeout(()=>{this.isOpen = false;this.isClosing=false},350)
+      return      
+    }
+    this.isClosing = false
     this.isOpen = !this.isOpen
+    setTimeout(()=>{this.hasOpened = this.isOpen},350)
   }
 }
